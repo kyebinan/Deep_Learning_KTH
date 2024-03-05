14d00fdf214a24a53acffe3913226fd206c8aa96
yohan <yohanbinan22@gmail.com>
2024-03-05 14:12:43 +0100

    Done subsampling the data

--- LAB1A/delta_rule.py
+++ LAB1A/delta_rule.py
@@ -83,6 +83,42 @@ def delta_learning_batch(X, y, epochs = 50, learning_rate = 0.001, batch_size=10
     return W, errors
 
 
+def delta_learning_batch_without_bias(X, y, epochs = 50, learning_rate = 0.001, batch_size=10):
+    """
+    Train a linear regression model using the Delta learning rule in batch mode without bias.
+
+    Parameters:
+    - X (numpy.ndarray): Input features of shape (m, n), where m is the number of samples and n is the number of features.
+    - y (numpy.ndarray): True labels of shape (m,), where m is the number of samples.
+    - epochs (int): Number of training epochs (default: 50).
+    - learning_rate (float): Learning rate for adjusting model parameters (default: 0.001).
+    - batch_size (int): Size of the batches for batch updates (default: 10).
+
+    Returns:
+    - tuple: Lists containing weights and errors at each epoch during training.
+      - W (list): List of updated weights at each epoch.
+      - errors (list): List of mean squared errors at each epoch.
+    """
+    weights = np.random.rand(X.shape[1]+1)
+    A = np.zeros((X.shape[0],1))
+    X = np.concatenate((X, A), axis=1)
+    W = []
+    n = X.shape[0]//batch_size
+    errors = []
+    for _ in range(epochs):
+        for k in range(n):
+            X_batch = X[k*batch_size : (k+1)*batch_size, :]
+            y_batch = y[k*batch_size : (k+1)*batch_size]
+            weights = delta_learning_batch_step(weights, X_batch, y_batch, learning_rate=learning_rate)
+            W.append(weights)
+        err = loss_function(X,y, weights)
+        errors.append(err)
+    return W, errors
+
+
+
+
+
 def main():
     print("delta rule module")
 
