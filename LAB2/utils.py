84b31576308543f8be22da365b02c86fc634ab59
yohan <yohanbinan22@gmail.com>
2024-03-10 01:46:12 +0100

    I've done the competitive learning function

--- LAB2/utils.py
+++ LAB2/utils.py
@@ -30,6 +30,57 @@ def threshold_function(x, threshold=0):
     return np.where(x >= threshold, 1, -1)
 
 
+def competitive_learning(data, num_rbf_units, learning_rate_cl, epochs_cl, num_winners=3):
+    """Trains a Competitive Learning (CL) network with Radial Basis Function (RBF) units on the input data.
+
+    Parameters:
+    - data (numpy.ndarray): Input data for training the CL network. Should be a 2D array.
+    - num_rbf_units (int): Number of RBF units in the CL network.
+    - learning_rate_cl (float): Learning rate for adjusting RBF node weights during training.
+    - epochs_cl (int): Number of training epochs for the CL network.
+    - num_winners (int): Number of winning units to be updated during each training step.
+
+    Returns:
+    numpy.ndarray:
+        The trained RBF nodes representing the learned feature space.
+
+    Raises:
+    ValueError: If the input data is not a 2D array.
+
+    The function uses competitive learning to train the RBF network. It initializes random RBF nodes and
+    iteratively updates them based on the input data. The goal is to have each RBF node represent a region
+    of the input space and adjust its weights to become closer to the training vectors.
+    """
+
+     # Check if data is 1D and reshape if necessary
+    if len(data.shape) == 1:
+        data = data.reshape(-1, 1)
+
+    # Ensure data has the correct shape
+    if len(data.shape) != 2:
+        raise ValueError("Input data should be a 2D array.")
+
+    num_features = data.shape[1]
+    rbf_nodes = np.random.rand(num_rbf_units, num_features)
+
+    for _ in range(epochs_cl):
+        np.random.shuffle(data)
+
+        for sample in data:
+             # Find the winning units (closest RBF nodes)
+            distances = np.linalg.norm(rbf_nodes - sample, axis=1)
+            winning_units = np.argsort(distances)[:num_winners]
+
+            # Update the winning units to get closer to the training vector
+            for winning_unit in winning_units:
+                rbf_nodes[winning_unit] += learning_rate_cl * (sample - rbf_nodes[winning_unit])
+
+    return rbf_nodes
+
+
+
+
+
 def main():
     print("utils module")
 
